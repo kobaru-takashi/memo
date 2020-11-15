45fad74137bf9cb6be96d80a3981348f4caf1f8a
kobaru-takashi <ta.a.1445.1@gmail.com>
2020-11-15 19:33:57 +0900

    ページネーション

--- index.php
+++ index.php
@@ -18,19 +18,39 @@
 <main>
 <h2>Practice</h2>
 <?php
-try {
-    $db = new PDO('mysql:dbname=mydb;host=localhost;port=8889;charset=utf8','root','root');
-} catch(PDOException $e) {
-    echo 'DB接続エラー:' . $e->getMessage();
+require('dbconnect.php');
+
+if (isset($_REQUEST['page']) && is_numeric($_REQUEST['page'])){
+    $page =$_REQUEST['page'];
+} else {
+    $page = 1;
 }
-$memos =$db->query('SELECT * FROM memos ORDER BY id DESC');
+$start = 5 * ($page - 1);
+
+$memos = $db->prepare('SELECT * FROM memos ORDER BY id DESC LIMIT ?, 5');
+$memos->bindParam(1, $start, PDO::PARAM_INT);
+$memos->execute();
 ?>
+
 <article>
     <?php while($memo = $memos->fetch()): ?>
       <p><a href="memo.php?id=<?php print($memo['id']); ?>"><?php print(mb_substr($memo['memo'], 0, 50)); ?></a></p>
       <time><?php print($memo['created_at']); ?></time>
       <hr>
     <?php endwhile; ?>
+
+    <?php if ($page >= 2): ?>
+        <a href="index.php?page=<?php print($page-1); ?>"><?php print($page-1); ?>ページ目へ</a>
+    <?php endif; ?>
+     |
+    <?php
+    $counts = $db->query('SELECT COUNT(*) as cnt FROM memos');
+    $count = $counts->fetch();
+    $max_page = ceil($count['cnt'] / 5);
+    if ($page < $max_page):
+    ?>
+        <a href="index.php?page=<?php print($page+1); ?>"><?php print($page+1); ?>ページ目へ</a>
+    <?php endif; ?>
 </article>
 </main>
 </body>    
--- input_do.php
+++ input_do.php
@@ -19,21 +19,14 @@
 <h2>Practice</h2>
 <pre>
 <?php
-try {
-    $db = new PDO('mysql:dbname=mydb;host=localhost;port=8889;charset=utf8','root','root');
-
-    $statement = $db->prepare('INSERT INTO memos SET memo=?, created_at=NOW()');
-    $statement->bindParam(1, $_POST['memo']);  //①
-    $statement->execute();                    //②
-    // $statement->execute(array($_POST['memo'])); ③＝①＋②同じ内容
-    echo 'メッセージが登録されました';
-} catch(PDOException $e) {
-    echo 'DB接続エラー:' . $e->getMessage();
-}
-
-
+  require('dbconnect.php');
+  $statement = $db->prepare('INSERT INTO memos SET memo=?, created_at=NOW()');
+  $statement->bindParam(1, $_POST['memo']);  //①
+  $statement->execute();                    //②
+  // $statement->execute(array($_POST['memo'])); ③＝①＋②同じ内容
+  echo 'メッセージが登録されました';
 ?>
-  </pre>
+</pre>
 </main>
 </body>    
 </html>
